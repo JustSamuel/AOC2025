252e00fb60cef01b8899a584a6080a567eda0564
Samuel <samuelincognito@gmail.com>
2025-12-03 06:30:20 +0100

    feat: day 3
    Slow start, but amazing delta by having the benefit that my part 1 approach was easily enough to adapt to part 2.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -76,16 +76,16 @@ Both parts: 7.689ms
 
 ```
 Day 02
-Time part 1: 198.429ms
-Time part 2: 362.927ms
-Both parts: 561.356ms
+Time part 1: 190.072ms
+Time part 2: 353.854ms
+Both parts: 543.926ms
 ```
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 5.471ms
+Time part 2: 1.551ms
+Both parts: 7.022ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 569.044ms
+Total stars: 6/50
+Total time: 558.637ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.ts
@@ -0,0 +1,69 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => {
+  return rawInput.trim().split("\n").map((line) => line.split("").map(Number));
+};
+
+const findMax = (bank: number[], batteryCount: number): number => {
+  const n = bank.length;
+  const k = batteryCount;
+  const selected: number[] = [];
+  let start = 0;
+
+  for (let pos = 0; pos < k; pos++) {
+    const remaining = k - pos - 1;
+    const end = n - remaining;
+    const slice = bank.slice(start, end)
+
+    const digit = Math.max(...slice);
+    const index = start + slice.indexOf(digit);
+
+    selected.push(digit);
+    start = index + 1;
+  }
+
+  return Number(selected.join(""));
+};
+
+const solver = (banks: number[][], batteryCount: number) => {
+  let sum = 0;
+
+  for (const bank of banks) {
+    sum += findMax(bank, batteryCount);
+  }
+
+  return sum;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+        `,
+        expected: 357,
+      },
+    ],
+    solution: (rawInput: string) => solver(parseInput(rawInput), 2),
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+        987654321111111
+811111111111119
+234234234234278
+818181911112111
+        `,
+        expected: 3121910778619,
+      },
+    ],
+    solution: (rawInput: string) => solver(parseInput(rawInput), 12),
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
